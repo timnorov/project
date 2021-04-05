3cc8d380cf6c7d886c1dc8c19b2e141b23ae1720
timnorov <timur.norov@gmail.com>
2021-04-05 23:10:19 +0300

    Результат урока 2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let misson;
-let period;
+let money = 50000;
+let income = 'обучение';
+let addExpenses = 'Ипотека, Такси, ЖКХ';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
 
-alert('Hello, world!')
-console.log('Hello, world!');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(' '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
